756e449741f47a7947b2383834d6578d5d4a6438
DilipSha04 <imdilip.004@gmail.com>
2023-12-30 14:50:18 +0530

    add mobile toggle menu feature

--- src/Componants/MobileMenu.js
+++ src/Componants/MobileMenu.js
@@ -4,19 +4,17 @@ import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
-import { LOGO, MENU_ICON_URL, SUPPORTED_LANGUAGES } from "../utils/constants";
+import { SUPPORTED_LANGUAGES } from "../utils/constants";
 import { toggelGptSearchView } from "../utils/gptSlice";
 import lang from "../utils/languageConstant";
 import { changeLanguage } from "../utils/configSlice";
-import MobileMenu from "./MobileMenu";
-import { toggleMenu } from "../utils/menuSlice";
 
-
-const Header = () => {
+const MobileMenu = () => {
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
+  const isMenuOpen = useSelector((store) => store.menu.isMenuOpen);
 
   const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
@@ -59,51 +57,41 @@ const Header = () => {
   const handleLanguageChange = (e) => {
     dispatch(changeLanguage(e.target.value));
   };
-
-  const handletoggleMenu = () => {
-    dispatch(toggleMenu())
-  }
-
+  if(!isMenuOpen) return null;
   return (
-    <div className="w-full absolute bg-gradient-to-b from-black z-10 bg-black md:py-0 py-2 h-24 ">
-      <img className="md:w-[14rem] w-[10rem] md:ml-2 ml-3 mt-0 contrast-125" src={LOGO} alt="Logo" />
-      <MobileMenu />
-      {user && (
-        <div className="absolute right-3 md:top-6 top-6 flex">
-          <img className="bg-red-700 w-10 rounded-sm md:hidden inline-block" src={MENU_ICON_URL} onClick={handletoggleMenu} alt="Menu" />
-          <div className="md:flex justify-between hidden">
-          {showGptSearch && <select
-            className="outline-none bg-[#e50314] px-2  text-white "
+    <div className="md:hidden flex relative">
+      <div className="flex flex-col justify-center  space-y-4 py-2 fixed w-5/12 h-[50%] right-0 top-0 bg-black bg-opacity-80 text-white ">
+        {showGptSearch && (
+          <select
+            className="outline-none rounded-sm bg-[#e50314] px-2 py-2 mx-2  text-white "
             onChange={handleLanguageChange}
           >
             {SUPPORTED_LANGUAGES.map((lang) => (
-              <option className="text-[12px] md:text-[16px] py-0 md:py-1 px-0" key={lang.identifier} value={lang.identifier}>
+              <option
+                className="text-[12px] py-0 md:py-1 px-0"
+                key={lang.identifier}
+                value={lang.identifier}
+              >
                 {lang.name}
               </option>
             ))}
-          </select>}
-          <button
-            className="px-4 py-2 mx-4 bg-[#e50314] text-white rounded-sm"
-            onClick={handleGptSearch}
-          >
-            {showGptSearch ? "Home" : "GPT Search"}
-          </button>
-          <img
-            className="w-10 rounded-sm"
-            src={user?.photoURL}
-            alt="userProfile"
-          />
-          <button
-            className="bg-[#e50914] text-white text-[16px] mx-4 px-2 rounded-sm"
-            onClick={handleSignOut}
-          >
-            Sign Out
-          </button>
-          </div>
-        </div>
-      )}
+          </select>
+        )}
+        <button
+          className="px-4 py-2 mx-2 bg-[#e50314] text-white rounded-sm"
+          onClick={handleGptSearch}
+        >
+          {showGptSearch ? "Home" : "GPT Search"}
+        </button>
+        <button
+          className="bg-[#e50914] text-white text-[16px] mx-2 py-2 px-4 rounded-sm"
+          onClick={handleSignOut}
+        >
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Header;
+export default MobileMenu;
